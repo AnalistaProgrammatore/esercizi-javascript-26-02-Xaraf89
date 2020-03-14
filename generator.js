f7772272d2316900ebd4f8ffc6815803d9430ce8
Xaraf89 <59976249+Xaraf89@users.noreply.github.com>
2020-03-14 16:38:33 +0100

    aggiornamento

--- generator.js
+++ generator.js
@@ -1,1 +1,18 @@
+//dato un array preesistente fai in modo tramite prompt di aggiungere numeri
+
+let a, b, rest;
+var ripeti, numeriAggiunti = [];
+while (ripeti !== "n") {
+    numeriAggiunti.push(prompt("aggiungi il numero all'array"));
+    ripeti = prompt('vuoi continuare a scrivere numeri y/n');
+    console.log(numeriAggiunti + " ");
+}
+
+function numbers() {
+    rest = numeriAggiunti;
+    [a, b] = [10, 20];
+    console.log([a, b] + "," + rest);
+}
+numbers()
+
 
